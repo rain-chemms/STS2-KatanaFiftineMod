f29a13351c712d4340e9da32c6d4944166b6f602
Cheems <168400671+rain-chemms@users.noreply.github.com>
2026-05-12 17:16:55 +0800

    Add [Card] RemoteBomb , CreateBomb

---
+++ FiftineCode/Cards/Token/RemoteBomb.cs
@@ -0,0 +1,48 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using Godot;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KatanaZeroMod.Fiftine.Cards;
+
+//遥控炸弹
+public class RemoteBomb() : FiftineCard(3,
+    CardType.Attack, CardRarity.Token,
+    TargetType.AllEnemies)
+{
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.None];
+    public override HashSet<CardKeyword> CanonicalKeywords => new HashSet<CardKeyword> {
+        CardKeyword.Exhaust,
+        CardKeyword.Retain
+    };
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(10,ValueProp.Move)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromKeyword(CardKeyword.Exhaust),
+        HoverTipFactory.FromKeyword(CardKeyword.Retain)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CommonActions.CardAttack(this, cardPlay.Target).Execute(choiceContext);
+    }
+
+    protected override Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
+    {
+        DynamicVars.Damage.UpgradeValueBy(DynamicVars.Damage.ToUInt32(null));
+        return Task.CompletedTask;
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.MockSetEnergyCost(new CardEnergyCost(this, 2, false));
+    }
+}
---
+++ FiftineCode/Cards/Uncommon/CreateBomb.cs
@@ -0,0 +1,54 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using Godot;
+using KatanaZeroMod.Fiftine.Cards;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Modifiers;
+using MegaCrit.Sts2.Core.Models.Potions;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.ValueProps;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace KatanaZeroMod.Fiftine.Cards;
+
+public class CreateBomb() : FiftineCard(1,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    public override HashSet<CardKeyword> CanonicalKeywords => new HashSet<CardKeyword> {
+        CardKeyword.Exhaust
+    };
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.None];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromCard<RemoteBomb>(),
+        HoverTipFactory.FromKeyword(CardKeyword.Exhaust)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        RemoteBomb remoteBomb = new RemoteBomb();
+        if (base.IsUpgraded)
+        {
+            CardCmd.Upgrade(remoteBomb);
+        }
+        await CardPileCmd.AddGeneratedCardToCombat(remoteBomb, PileType.Hand, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.MockSetEnergyCost(new CardEnergyCost(this, 0, false));
+    }
+}
+
